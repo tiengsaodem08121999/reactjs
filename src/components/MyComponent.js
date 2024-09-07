552be1686f3c660d2efa13cdae3a06364d008c2a
QuyLV <quylv@splus-software.com.vn>
2024-09-07 09:57:03 -0700

    state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: "QuyLV",
+        address: "HCM",
+        age: 25
+      };
     render() {
         return (
-            <div>my first Component</div>
+            <div> My name is {this.state.name} I'm from {this.state.address}</div>
         );
     }
 }
