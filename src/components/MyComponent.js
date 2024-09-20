091d1ad4d0c3be9c97a319d408fceea17cbfa068
QuyLV <quylv@splus-software.com.vn>
2024-09-20 17:50:37 +0700

    form

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,10 +5,41 @@ class MyComponent extends React.Component {
         name: "QuyLV",
         address: "HCM",
         age: 25
-      };
+    };
+    handleClick = (event) => {
+        console.log("my name: ", this.state.name);
+
+        this.setState({
+            name: "Le Van Quy",
+            age: Math.floor((Math.random() * 100) + 1),
+        })
+    }
+
+    handleOnMoveOver(event) {
+        // console.log(event);
+
+    }
+
+    handleOnChangeInput = (event) => {
+        this.setState({
+            name: event.target.value,
+        })
+    }
+
+    handleonSubmit = (event) => {
+        event.preventDefault();
+        alert('me');
+    }
+
     render() {
         return (
-            <div> My name is {this.state.name} I'm from {this.state.address}</div>
+            <div>
+                My name is {this.state.name} I'm  {this.state.age}
+                <form onSubmit={(event) => this.handleonSubmit(event)}> 
+                    <input type="text" onChange={(event) => this.handleOnChangeInput(event)}/>
+                    <button>Submit</button>
+                </form>
+            </div>
         );
     }
 }
