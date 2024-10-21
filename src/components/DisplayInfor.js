beeacb717b80e002a5dfd7ff6a9454dfb0d6feae
QuyLV <quylv@splus-software.com.vn>
2024-10-21 15:11:29 +0700

    stateless and state ful

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,64 +2,58 @@ import React from "react";
 import './DisplayInfor.scss';
 import logo from './../logo.svg';
 
-class DisplayInfor extends React.Component {
-    constructor(props) {
-        console.log("call me constructor: 1");
+//stateless vs stateful
 
-        super(props);
+// class DisplayInfor extends React.Component {
 
-        this.state = {
-            isShow: true,
-        };
-    }
+//     render() {
+//         const { listUsers } = this.props;
+//         console.log("call me render");
 
-    componentDidMount() {
-        console.log("call me component did mount: 2");
-        setTimeout(() => { document.title = "QuylV" }, 3000);
-    }
+//         return (
+//             <div className="display-infor-container">
+//                 {true &&
+//                     <div>
+//                         {listUsers.map((user) => {
+//                             return (
+//                                 <div key={user.id}
+//                                     className={+user.age >= 18 ? 'red' : 'blue'} >
+//                                     <div>My name {user.name}</div>
+//                                     <div>My Age {user.age}</div>
+//                                     <button onClick={() => this.props.handleDeletUser(user.id)}>Delete</button>
+//                                     <hr></hr>
+//                                 </div>
+//                             )
+//                         })}
+//                     </div>
+//                 }
+//             </div>
+//         )
+//     }
+// }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log("call me update", this.props,  prevProps);
-        if(this.props.listUsers !== prevProps.listUsers) {
-            if(this.props.listUsers.length === 5) {
-                alert(' You got 5 user !');
+const DisplayInfor = (props) => {
+    const { listUsers } = props;
+
+    return (
+        <div className="display-infor-container">
+            {true &&
+                <div>
+                    {listUsers.map((user) => {
+                        return (
+                            <div key={user.id}
+                                className={+user.age >= 18 ? 'red' : 'blue'} >
+                                <div>My name {user.name}</div>
+                                <div>My Age {user.age}</div>
+                                <button onClick={() => props.handleDeletUser(user.id)}>Delete</button>
+                                <hr></hr>
+                            </div>
+                        )
+                    })}
+                </div>
             }
-        }
-
-    }
-
-    handleShowHide = () => {
-        this.setState({
-            isShow: !this.state.isShow
-        });
-    }
-
-    render() {
-        const { listUsers } = this.props;
-        console.log("call me render");
-
-
-        return (
-            <div className="display-infor-container">
-                <div> <button onClick={() => { this.handleShowHide() }}> {this.state.isShow ? 'Hide' : 'show'} list user</button> </div>
-                {this.state.isShow &&
-                    <div>
-                        {listUsers.map((user) => {
-                            return (
-                                <div key={user.id}
-                                    className={+user.age >= 18 ? 'red' : 'blue'} >
-                                    <div>My name {user.name}</div>
-                                    <div>My Age {user.age}</div>
-                                    <button onClick={() => this.props.handleDeletUser(user.id)}>Delete</button>
-                                    <hr></hr>
-                                </div>
-                            )
-                        })}
-                    </div>
-                }
-            </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default DisplayInfor;
